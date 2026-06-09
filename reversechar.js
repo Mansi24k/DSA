76a2193fbb860e0026630199c5df349f1818e636
Mansi24k <mansikrishnapurkar@gmail.com>
2026-06-09 15:44:51 +0530

    9June26

--- reversechar.js
+++ reversechar.js
@@ -1,32 +1,44 @@
+//mine
+// function reverseString (s) {
+//     let x = s.length-1;
+//     let xcopy;
+    
+//         if(s.length%2 == 0){
+//             for(let i=0;i<s.length/2;i++){
+//             if(x<s.length/2){
+//                break;
+//             }else{
+//                 xcopy = s[i];
+//                 s[i] = s[x];
+//                 s[x] = xcopy;
+//                 x--;
+//             }
+//         }
+//         }else{
+//             for(let i=0;i<s.length;i++){
+//             if(i==x){
+//                 break;
+//             }else{
+//                 xcopy = s[i];
+//                 s[i] = s[x];
+//                 s[x] = xcopy;
+//                 x--;
+//             }
+//         }
+    
+//     }
+//     return s;
+// };
+// console.log(reverseString(["H","a","n","n","a","h"]))
 
-function reverseString (s) {
+//sirs
+ function reverseString (s) {
     let x = s.length-1;
-    let xcopy;
-    
-        if(s.length%2 == 0){
-            for(let i=0;i<s.length/2;i++){
-            if(x<s.length/2){
-               break;
-            }else{
-                xcopy = s[i];
-                s[i] = s[x];
-                s[x] = xcopy;
-                x--;
-            }
-        }
-        }else{
-            for(let i=0;i<s.length;i++){
-            if(i==x){
-                break;
-            }else{
-                xcopy = s[i];
-                s[i] = s[x];
-                s[x] = xcopy;
-                x--;
-            }
-        }
-    
+    for(let i=0; i<s.length/2;i++){
+        let temp = s[i];
+        s[i] = s[x-i];
+        s[x-i] = temp;
     }
-    return s;
+    return s;    
 };
-console.log(reverseString(["H","a","n","n","a","h"]))
+console.log(reverseString(["H","a","n","n","a"]))
